f1895ffc25e4868d119fde661651da6411e140e3
Dishank Hanwate <dishank.hanwate@zeuslearning.com>
2022-07-01 12:44:22 +0530

    .

---
+++ practice/angular course/my-new-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent,
+    CustomersListComponent,
+    
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+  
