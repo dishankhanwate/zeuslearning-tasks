0684c4b488fe2e5266cd91a15849626dcabdbb7d
Dishank Hanwate <dishank.hanwate@zeuslearning.com>
2022-07-06 17:28:51 +0530

    navbar actives

---
+++ Quantum with Angular/angular-quantum/src/app/core/alert.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  baseUrl: string = 'assets/'
+  constructor() { }
+}
---
+++ Quantum with Angular/angular-quantum/src/app/navbar/navbar.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  isActiveAlert=false;
+  isActiveAnnouncement=false;
+  isActiveHam=false;
+  isActiveDashboard=false;
+  isActiveContent=false;
+  isActiveUsers=false;
+  isActiveReports=false;
+  isActiveAdmin=false;
+
+  changeActiveAlert(){
+    this.isActiveAlert= !this.isActiveAlert;
+
+    if (this.isActiveAlert){
+      this.isActiveAnnouncement=false;
+      this.isActiveHam=false;
+    }
+  }
+
+  changeActiveAnnouncement(){
+    this.isActiveAnnouncement= !this.isActiveAnnouncement;
+    if (this.isActiveAnnouncement) {
+      this.isActiveAlert=false;
+      this.isActiveHam=false
+    }
+  }
+
+  changeActiveHam(){
+    this.isActiveHam= !this.isActiveHam;
+    if (this.isActiveHam) {
+      this.isActiveAlert=false;
+      this.isActiveAnnouncement=false
+    }
+  }
+
+  changeActiveDashboard(){
+    this.isActiveDashboard= !this.isActiveDashboard;
+    if (this.isActiveDashboard){
+      this.isActiveContent=false;
+      this.isActiveUsers=false;
+      this.isActiveReports=false;
+      this.isActiveAdmin=false;
+    }
+  }
+
+  changeActiveContent(){
+    this.isActiveContent= !this.isActiveContent;
+    if (this.isActiveContent){
+      this.isActiveDashboard=false;
+      this.isActiveUsers=false;
+      this.isActiveReports=false;
+      this.isActiveAdmin=false;
+    }
+  }
+
+  changeActiveUsers(){
+    this.isActiveUsers= !this.isActiveUsers;
+    if (this.isActiveUsers){
+      this.isActiveContent=false;
+      this.isActiveDashboard=false;
+      this.isActiveReports=false;
+      this.isActiveAdmin=false;
+    }
+  }
+
+  changeActiveReports(){
+    this.isActiveReports= !this.isActiveReports;
+    if (this.isActiveReports){
+      this.isActiveContent=false;
+      this.isActiveUsers=false;
+      this.isActiveDashboard=false;
+      this.isActiveAdmin=false;
+    }
+  }
+
+  changeActiveAdmin(){
+    this.isActiveAdmin= !this.isActiveAdmin;
+    if (this.isActiveAdmin){
+      this.isActiveContent=false;
+      this.isActiveUsers=false;
+      this.isActiveReports=false;
+      this.isActiveDashboard=false;
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
