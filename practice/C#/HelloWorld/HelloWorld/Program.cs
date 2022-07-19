da18404cce260558a886fa1b911573ec160ac731
Dishank Hanwate <dishank.hanwate@zeuslearning.com>
2022-07-19 14:34:50 +0530

    aspdotnet practice

---
+++ practice/C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1,"John ");
+                               Console.WriteLine(customer.Id);
+  Console.WriteLine(customer.Name);
+
+            var names = new List<string>();
+            foreach (var name in names)
+            {
+                var a = 9;
+            }
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
+ 
